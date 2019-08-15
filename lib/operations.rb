ca8b90a8393341b51e79325258a95ecaf196d32c
Kierra M <k.merriman32@hotmail.com>
2019-08-15 13:42:08 +0000

    Automatically backed up by Learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,7 +1,7 @@
 def unsafe?(speed)
  if speed < 40 || speed > 60
    true
- else
+ else 
    false
 end
 end
@@ -9,7 +9,7 @@ end
 
 
 def not_safe?(speed)
-	 speed < 40 || speed > 60 ? "true" : "false"
+	 speed < 40 || speed > 60 ?  true : false
 end
 
 
